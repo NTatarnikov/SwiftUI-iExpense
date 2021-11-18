101938122b4c4d18ca0815cb2cbde032eceedf55
NTatarnikov <t4t4rn1k0v@gmail.com>
2021-11-18 21:48:41 +0300

    Initial Commit

---
+++ SwiftUI iExpense/SwiftUI_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_iExpenseApp.swift
+//  SwiftUI iExpense
+//
+//  Created by Nikita Tatarnikov on 18.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
