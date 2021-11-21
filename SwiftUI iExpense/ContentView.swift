b07ea09e71e5e5f562c67a3952fac16a8f85234b
NTatarnikov <t4t4rn1k0v@gmail.com>
2021-11-21 22:52:18 +0300

    Added a different colors for currency and changes currency to user's preferred currency

--- SwiftUI iExpense/ContentView.swift
+++ SwiftUI iExpense/ContentView.swift
@@ -25,7 +25,9 @@ struct ContentView: View {
                         
                         Spacer()
                         
-                        Text(item.amount, format: .currency(code: "USD"))
+                        Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                            .foregroundColor(item.amount > 999 ? .red : item.amount > 99 ? .orange : .green)
+                            
                     }
                 }
                 .onDelete(perform: removeItems)
